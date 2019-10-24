8913c3dc02b5e573c6b8a965a86b0072c8fe2e41
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-10-24 15:47:50 +0200

    Add project files.

---
+++ CBit/main.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "superlogger.h"
+
+
+
+void main()
+{
+	WriteLogMessage("Nu startar programmet",
+		false, true, true,
+		false,
+		true,
+		false);
+
+}
---
+++ CBit/superlogger.h
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+
+
+void WriteLogMessage(char* message,
+	bool toFile, bool toConsole, bool includeDate,
+	bool includeDayOfWeek,
+	bool includeTime,
+	bool uppercaseMessage);
